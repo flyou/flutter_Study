3d5ff8a1a42988f291ee9085ac64d20ce2efa5c6
flyou <fang553274238>
2018-04-16 17:09:51 +0800

    调用平台代码

--- android/app/src/main/java/com/yourcompany/test1/MainActivity.java
+++ android/app/src/main/java/com/yourcompany/test1/MainActivity.java
@@ -1,14 +1,33 @@
 package com.yourcompany.test1;
 
 import android.os.Bundle;
-
+import android.text.TextUtils;
+import android.widget.Toast;
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    private static final String CHANNEL = "samples.flutter.io/toast";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+        new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+                new MethodChannel.MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+                        System.out.println(call.argument("msg").toString());
+                        if (call.method.equals("showToast")) {
+                            if (call.hasArgument("msg")&& !TextUtils.isEmpty(call.argument("msg").toString())) {
+                                Toast.makeText(MainActivity.this,call.argument("msg").toString() , Toast.LENGTH_SHORT).show();
+                            }else {
+                                Toast.makeText(MainActivity.this,"toast text must not null" , Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
 }
---
+++ lib/platform_channels/android_toast.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  static const platform = const MethodChannel("samples.flutter.io/toast");
+   showToast(String msg) async {
+    try {
+    await platform.invokeMethod("showToast",{"msg":msg});
+    } on PlatformException catch (e) {
+     print(e.toString());
+    }
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("platformChannels"),),body:  Center(
+      child: RaisedButton(
+        child: Text("点我提示"),
+        onPressed: () {
+          showToast("");
+        },
+      ),
+    ),);
+  }
+}
